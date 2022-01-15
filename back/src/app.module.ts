5184f45b009f8bc959fd1aac19df1b66023e0ef0
Igor <kudlaev.igor@gmail.com>
2022-01-15 17:28:45 +0300

    feat: added auth and users

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT, () =>
+    console.log(`App has been started on ${PORT} port`),
+  );
 }
 bootstrap();
